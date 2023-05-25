435e3804b59b6d4ec43bb4d05ed826ffe2b2f0f9
ibrahimfqaisi <qasrawi.ibrahim1998@gmail.com>
2023-05-25 18:50:55 +0000

    we apdate  rool in our boot

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,4 +1,8 @@
-from ten_thousand.game_logic import GameLogic
+try:
+    from ten_thousand.game_logic import GameLogic
+except:
+    from game_logic import GameLogic
+    
 def play():
     GameLogic().play_dice()
     
@@ -6,4 +10,6 @@ class Game(GameLogic):
     def play(self):
          GameLogic().play_dice()
    
-    
+
+if __name__ == "__main__":
+    play()
